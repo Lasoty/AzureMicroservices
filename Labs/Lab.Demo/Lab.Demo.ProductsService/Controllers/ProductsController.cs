41ed43e4be779bbf791a554247d53575aeb54a89
Leszek Lewandowski <lasoty@o2.pl>
2025-06-28 23:05:28 +0200

    Lab3 - Azure Service Bus

---
+++ Labs/Lab.Demo/Lab.Demo.ProductsService/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Demo.ProductsService.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsController : ControllerBase
+{
+
+	[HttpGet]
+	[EndpointSummary("Zwraca listę produktów")]
+	public IActionResult Get() => Ok(new { Source = "ProductService", Products = (string[]) ["Product1", "Product2"] });
+}
