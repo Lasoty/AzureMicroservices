13489441ba59e96b9a2b12a9567f6ab4bb013d59
Leszek Lewandowski <lasoty@o2.pl>
2025-06-28 22:49:09 +0200

    Lab2 - Api Contracts Initial

---
+++ Labs/Lab.Demo/Lab.Demo.ProductsService/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Demo.ProductsService.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult Get() => Ok(new { Source = "ProductService", Products = (string[]) ["Product1", "Product2"] });
+
+
+	[HttpGet("{id}")]
+	public IActionResult Get(int id) => Ok(new { Source = "ProductService", Product = (string)"Product1" });
+
+	[HttpPost]
+	public IActionResult Post([FromBody] string product)
+	{
+		if (string.IsNullOrEmpty(product))
+		{
+			return BadRequest("Product cannot be null or empty.");
+		}
+		return CreatedAtAction(nameof(Get), new { id = 1 }, new { Source = "ProductService", Product = product });
+	}
+
+	[HttpPut("{id}")]
+	public IActionResult Put(int id, [FromBody] string product)
+	{
+		if (string.IsNullOrEmpty(product))
+		{
+			return BadRequest("Product cannot be null or empty.");
+		}
+		return Ok(new { Source = "ProductService", Product = product });
+	}
+
+	[HttpDelete("{id}")]
+	public IActionResult Delete(int id)
+	{
+		if (id <= 0)
+		{
+			return BadRequest("Invalid product ID.");
+		}
+		return NoContent();
+	}
+}
