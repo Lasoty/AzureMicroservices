d21c9306444ffaf026a2b3e11caed15c879a9cf8
Leszek Lewandowski <lasoty@o2.pl>
2025-06-30 01:04:37 +0200

    Lab8 - Docker compose

---
+++ Labs/Lab.Demo/Lab.Demo.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab.Demo.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
